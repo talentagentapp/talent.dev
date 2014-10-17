8b63fd06439fd0b2f225571a0e14a8a3052c66a7
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-17 15:14:03 -0500

    removed redundant 'role type', added an enum for experience levels

--- app/database/migrations/2014_10_16_221452_create_users_table.php
+++ app/database/migrations/2014_10_16_221452_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
 
             //role_id will be set when creating account
             //number will correspond to type of role
-            $table->string('role_type', 128);
             $table->integer('role_id')->unsigned();
             
             $table->string('email', 128)->unique();
@@ -27,10 +26,10 @@ class CreateUsersTable extends Migration
             $table->string('password', 255);
             $table->string('first', 128);
             $table->string('last', 128);
+            $table->enum('experience', ['0-1', '1-5', '5-10', '10+']);
             $table->enum('sex', ['m', 'f', 'not say']);
             $table->rememberToken();
             $table->timestamps();
-
         });
     }
 
