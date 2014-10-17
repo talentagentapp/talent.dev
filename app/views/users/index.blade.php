1f5b535198b42fbe46b1b53898a93b8cba1aa11c
Audrey Lopez <audreyelopez@gmail.com>
2014-10-17 12:03:27 -0500

    Users index for iterating through collection of profiles in a given search

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,1 +1,23 @@
 /vagrant/sites/talent.dev/app/views/users/index.blade.php
+@extends('layouts.master')
+
+
+@section('body')
+
+	<div class="container">
+		{{{ Form::open() }}}
+
+		<div>
+			{{ Form::label('Search', 'search') }}
+		</div>	
+		<div>
+			{{ Form::text('search', Input::get('search')) }}
+		</div>	
+
+
+		{{{ Form::close() }}}
+	</div>
+
+    <!--Create logic below to iterate through individual user profiles-->
+
+@stop
