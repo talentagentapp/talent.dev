d408fed6e5dbbe2a132f627d6794e7fda088cc60
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-20 15:33:05 -0500

    updated the seeders

--- app/database/seeds/GroupsTableSeeder.php
+++ app/database/seeds/GroupsTableSeeder.php
@@ -3,16 +3,21 @@
 // Composer: "fzaninotto/faker": "v1.3.0"
 use Faker\Factory as Faker;
 
-class GroupsTableSeeder extends Seeder {
-
+class GroupsTableSeeder extends Seeder
+{
 	public function run()
 	{
+        DB::table('groups')->delete();
+
 		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
+		foreach(range(1, 5) as $index)
 		{
 			Group::create([
-
+                'name'        => $faker->word,
+                'description' => $faker->paragraph($nbSentences = 3),
+                'img'         => $faker->imageUrl($width = 640, $height = 480),
+                'created_at'  => $faker->dateTimeThisYear($max = 'now'),
 			]);
 		}
 	}
