c911ee19df5c99c54d2b847ddfe793f159a661ea
Audrey Lopez <audreyelopez@gmail.com>
2014-10-20 09:39:30 -0500

    Updated user views.

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -1,1 +1,46 @@
-/vagrant/sites/talent.dev/app/views/users/create.blade.php
+@extends('layouts.master')
+
+@section('content')
+<h2>Create Your Profile</h2>
+
+{{ Form::open(array('action' => 'PostsController@store', 'files' => true, 'class' => 'form-horizontal', 'id' => "contact-form")) }}
+
+<div class='form-group'>
+	{{ Form::label('role', 'Your Role in the entertainment industry:') }}
+	{{ Form::select('role', array('Talent Agent / Manager', 'Talent')) }}
+</div>
+
+<div class='form-group'>	
+	{{ Form::label('user', 'Desired Username:', array('class' => 'cold-sm-0 control-label')) }}
+	{{ Form::text('user', Input::old('user'), array('class' => 'form-control')) }}
+
+	{{ Form::label('password', 'Desired Password:', array('class' => 'cold-sm-0 control-label')) }}
+	{{ Form::text('password', Input::old('password'), array('class' => 'form-control')) }}
+</div>
+
+<div class='form-group'>
+	<h3>Contact Information<h3>
+	{{ Form::label('first-name', 'First Name:', array('class' => 'cold-sm-0 control-label')) }}
+	{{ Form::text('first-name', Input::old('first-name'), array('class' => 'form-control')) }}
+
+	{{ Form::label('last-name', 'Last Name:', array('class' => 'cold-sm-0 control-label')) }}
+	{{ Form::text('last-name', Input::old('last-name'), array('class' => 'form-control')) }}
+
+	{{ Form::label('email', 'E-mail Address:', array('class' => 'cold-sm-0 control-label')) }}
+	{{ Form::text('email', Input::old('email'), array('class' => 'form-control')) }}
+</div>
+
+<div class='form-group'>
+	{{ Form::select('Gender', array('Male', 'Female')) }}
+</div>
+
+<div class="form-group">
+	<div class="col-sm-offset-0 col-sm-12">
+		<button>Submit Form</button>
+		{{-- Form::submit('Submit Form') --}}	
+	</div>
+</div>			
+
+{{ Form::close() }}
+
+@stop
--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,3 +1,16 @@
 /vagrant/sites/talent.dev/app/views/users/show.blade.php
 
 @extends('layouts.master')
+
+@section('body')
+
+	<article>
+		<h1>{{{ $profile->username }}}</h1>
+		<p>{{{ $user->bio }}}</p>
+	</article>
+
+@stop
+
+@section('footer')
+
+@stop
