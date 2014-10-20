3d76e3c1f1d3bb8bc961c37138103a53ac3ab356
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-20 15:32:12 -0500

    added comments and rating table

--- app/database/seeds/CommentsTableSeeder.php
+++ app/database/seeds/CommentsTableSeeder.php
@@ -7,12 +7,16 @@ class CommentsTableSeeder extends Seeder {
 
 	public function run()
 	{
+        DB::table('comments')->delete();
+
 		$faker = Faker::create();
 
 		foreach(range(1, 10) as $index)
 		{
 			Comment::create([
-
+                'rating'     => $faker->randomElement($array = [1, 2, 3, 4, 5]),
+                'comment'    => $faker->realText($maxNbChars = 200, $indexSize = 2),
+                'created_at' => $faker->dateTimeThisYear($max = 'now')
 			]);
 		}
 	}
