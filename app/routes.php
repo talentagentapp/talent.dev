b2d5587d77c154390bc510cb77a4af32b51877d6
Audrey Lopez <audreyelopez@gmail.com>
2014-10-22 17:44:43 -0500

    Uneccessary route removed

--- app/routes.php
+++ app/routes.php
@@ -10,11 +10,6 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get('/mock', function()
-{
-	return View::make('mock');
-});
-
 Route::get('/', 'HomeController@showLanding');
 
 
