2296ca260c1c2513473b97aba973a02b02b191de
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-22 13:16:26 -0500

    continued working on gigs

--- app/controllers/GigsController.php
+++ app/controllers/GigsController.php
@@ -3,172 +3,142 @@
 class GigsController extends \BaseController
 {
 
-	/**
-	 * Display a listing of gigs
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//*****future search feature
-		// $search = Input::get('search');
+    /**
+     * Display a listing of gigs
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //*****future search feature
+        // $search = Input::get('search');
 
-		// $query = Gig::with('agent');
-		// $gigs = Gig::all();
+        // $query = Gig::with('agent');
+        // $gigs = Gig::all();
 
-		return View::make('gigs.index')->with('gigs', $gigs);
-	}
+        return View::make('gigs.index')->with('gigs', $gigs);
+    }
 
-	/**
-	 * Show the form for creating a new gig
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return View::make('gigs.create');
-	}
+    /**
+     * Show the form for creating a new gig
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return View::make('gigs.create');
+    }
 
-	/**
-	 * Store a newly created gig in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$gig = new Gig();
+    /**
+     * Store a newly created gig in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $gig = new Gig();
 
-		return $this->saveGig($gig);
-	}
+        return $this->saveGig($gig);
+    }
 
-	/**
-	 * Display the specified gig.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$gig = Gig::findOrFail($id);
-		$agent_id = $gig->agent_id;
-		$agent = Agent::find($agent_id);
+    /**
+     * Display the specified gig.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $gig = Gig::findOrFail($id);
+        $agent_id = $gig->agent_id;
+        $agent = Agent::find($agent_id);
 
-		if(!$gig) {
-			App:abort(404);
-		}
+        if(!$gig) {
+            App:abort(404);
+        }
 
-		return View::make('gigs.show')->with('gig', $gig)->with('agent', $agent);
-	}
+        return View::make('gigs.show')->with('gig', $gig)->with('agent', $agent);
+    }
 
-	/**
-	 * Show the form for editing the specified gig.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$gig = Gig::find($id);
+    /**
+     * Show the form for editing the specified gig.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $gig = Gig::find($id);
 
-		return View::make('gigs.edit')->with('gig', $gig);
-	}
+        return View::make('gigs.edit')->with('gig', $gig);
+    }
 
-	/**
-	 * Update the specified gig in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{	
-		//**find or fail optional
-		$gig = Gig::find($id);
-		//write a 404 statement for fail
-		return $this->saveGig($gig);
-	}
+    /**
+     * Update the specified gig in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {   
+        //**find or fail optional
+        $gig = Gig::find($id);
+        //write a 404 statement for fail
 
-	/**
-	 * Remove the specified gig from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		Gig::destroy($id);
-		//flash confirmation message here.
+        return $this->saveGig($gig);
+    }
 
-		return Redirect::route('gigs.index');
-	}
-	protected function saveGig(Gig $gig)
-	{
-		$validator = Validator::make(Input::all(),Gig::$rules);
+    /**
+     * Remove the specified gig from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Gig::destroy($id);
 
-		if ($validator->fails()) {
-			//***error message needs to be updated with rules
+        //flash confirmation message here.
 
-			Session::flash('errorMessage', 'Your Gig must have a username, password...');
+        return Redirect::route('gigs.index');
+    }
 
-			Log::error('Gig validator failed', Input::all());
+    protected function saveGig(Gig $gig)
+    {
+        $validator = Validator::make(Input::all(), Gig::$rules);
 
-			return Redirect::back()->withInput();
+        if ($validator->fails()) {
+            Session::flash('errorMessage', 'Your Gig must have a name, description...');
+            Log::error('Gig validator failed', Input::all());
 
-			// ->withErrors($validator));
-		} else {
-			// this would pass the authenticated id if the user is already logged in
-			//$user->user_id = Auth::id();
+            return Redirect::back()->withInput()->withErrors($validator);
+        } else {
 
-			$gig->role_id = Input::get('role_id');
+            $gig->name        = Input::get('name');
+            $gig->description = Input::get('description');
+            $gig->date        = Input::get('date');
+            $gig->location    = Input::get('location');
+            //TODO: update to find the user that is writing this
+            // $gig->agent_id    = Auth::user();
 
-			$gig->group_id = Input::get('group_id');
+            if(Input::hasFile('image')) {
 
-            //role_type should be a drop down, which we will set independently
-			$gig->role_type = Input::get('role_type');
+                $file = Input::file('image');
+                $destination_path = public_path() . '/img/';
+                $filename = str_random(6) . '_' . $file->getClientOriginalName();
+                $uploadSuccess = $file->move($destination_path, $filename);
 
-			$gig->name = Input::get('name');
+                $user->img = '/img/' . $filename;
+            }
 
-			$gig->gig_desc = Input::get('description');
+            $gig->save();
 
-			$gig->gig_date = Input::get('date');
+            $message = 'Gig was successfully saved/updated';
 
-			$gig->location = Input::get('location');
+            Session::flash('successMessage', $message);
+            Log::info($message, Input::all());
 
-			$gig->agent_id = Input::get('agent_id');
-
-			if(Input::hasFile('image')) {
-
-				$file = Input::file('image');
-
-				$destination_path = public_path() . '/img/';
-
-				$filename = str_random(6) . '_' . $file->getClientOriginalName();
-
-				$uploadSuccess = $file->move($destination_path, $filename);
-
-				$user->image_name = '/img/' . $filename;
-
-			} else {
-				//rewrite to specify a default image filepath
-				$file = Input::file('image');
-
-				$destination_path = public_path() . '/img/';
-
-				$filename = str_random(6) . '_' . $file->getClientOriginalName();
-
-				$uploadSuccess = $file->move($destination_path, $filename);
-
-				$user->image_name = '/img/' . $filename;
-
-				$gig->save();
-
-				$message = 'Gig was successfully saved';
-
-				Session::flash('successMessage', $message);
-
-				Log::info('Gig was successfully saved', Input::all());
-
-				return Redirect::action('GigController@show',$user->id);
-			}
-		}
-	}
+            return Redirect::action('GigsController@show', $user->id);
+        }
+    }
 }
