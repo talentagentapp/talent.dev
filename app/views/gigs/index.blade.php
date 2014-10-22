e822e7410e4ccf980e3e126f91ef04e4f6e08333
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-22 15:42:43 -0500

    created a rudimentary index view of gigs

--- app/views/gigs/index.blade.php
+++ app/views/gigs/index.blade.php
@@ -1,1 +1,26 @@
-/vagrant/sites/talent.dev/app/views/gigs/index.blade.php
+@extends('layouts.master')
+
+@section('title', 'Gigs Index')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class='col-md-12'>
+            <h2>Available Gigs</h2>
+            <hr>
+
+            @forelse($gigs as $gig)
+            <h3>{{{ $gig->name }}}</h3>
+
+            <p>What: {{{ $gig->description }}}</p>
+
+            <p>Where: {{{ $gig->location }}}</p>
+
+            <p>When: {{{ $gig->date }}}</p>
+            @empty
+            <p>No mo' gigs.</p>
+            @endforelse
+        </div>
+    </div> 
+</div>     
+@stop
