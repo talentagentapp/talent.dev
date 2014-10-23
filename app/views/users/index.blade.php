022fc87d77da46a28ae41f2e89182b1f0aea91c4
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-23 14:03:49 -0500

    working

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -2,18 +2,22 @@
 
 @section('title', 'Users Index')
 
-@section('body')
+@section('content')
 <div class="container">
-	<div class='row'>
-		<div class='col-md-12'>
-			@forelse($users as $user)
-			<h3>{{{ $user->first . ' ' . $user->last }}}</h3>
-
-			<h6>{{{ $user->skills }}}</h6>
-			@empty
-			<p>No mo' users.</p>
-			@endforelse
-		</div>
-	</div>
+    @forelse($users as $user)
+    <div class='row'>
+        <div class='col-md-4'>
+            <img src="{{ $user->img }}" alt="">
+        </div>
+        <div class='col-md-8'>
+            <h3>{{{ $user->first . ' ' . $user->last }}}</h3>
+            <p>{{{ $user->experience }}} Years of Experience.</p>
+            <p>{{{ $user->bio }}}</p>
+        </div>
+    </div>
+    <hr>
+    @empty
+    <p>No mo' users.</p>
+    @endforelse
 </div>
 @stop
