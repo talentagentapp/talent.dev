a85c24bd0170b0de6025d1f54e8c98b4dfd430a2
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-27 14:40:45 -0500

    made anyone be able to use gigs

--- app/database/migrations/2014_10_17_163950_create_gigs_table.php
+++ app/database/migrations/2014_10_17_163950_create_gigs_table.php
@@ -6,33 +6,33 @@ use Illuminate\Database\Schema\Blueprint;
 class CreateGigsTable extends Migration
 {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('gigs', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('name', 128);
-			$table->text('description');
-			$table->string('location', 256);
-			$table->date('date');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gigs', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name', 128);
+            $table->text('description');
+            $table->string('location', 256);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('gigs');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('gigs');
+    }
 
 }
