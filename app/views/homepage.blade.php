5f5a73bacb2ac37309ad50f060b2795904122639
Audrey Lopez <audreyelopez@gmail.com>
2014-10-27 09:20:13 -0500

    changes to css

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -1,2 +1,39 @@
 @extends(layouts.master)
+<style>
+#bg
+  {
+    position: fixed; 
+    top: -50%; 
+    left: -50%; 
+    width: 200%; 
+    height: 200%;
+  }
+  #bg img
+  {
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    right: 0; 
+    bottom: 0; 
+    margin: auto; 
+    min-width: 50%;
+    min-height: 50%;
+  }
 
+  footer
+  {
+    margin: 50px 0;
+  }
+
+.col-sm-1
+{
+  color: white;
+}
+
+<!-- Sets background image  -->
+</style>
+
+/*<!-- <div id="bg">
+    <img src="/img/bright.jpg" alt="">
+  </div>
+ -->*/
