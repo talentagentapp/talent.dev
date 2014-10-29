e7c371a21ccef9bb593dac4260e9ec10e3238ba2
Audrey Lopez <audreyelopez@gmail.com>
2014-10-29 10:57:53 -0500

    Removed duplicate code blocks

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -20,20 +20,20 @@
 			<div class='form-group'>
 				{{ Form::label('first', 'First Name:', array('class' => 'col-sm-0 control-label')) }}
 				{{ Form::text('first', Input::old('first'), array('class' => 'form-control')) }}
-                @if($errors->has('first')) <div class="alert alert-danger" role="alert">{{ $errors->first('first') }}</div> @endif
+				@if($errors->has('first')) <div class="alert alert-danger" role="alert">{{ $errors->first('first') }}</div> @endif
 			</div>
 
 			<div class="form-group">
 				{{ Form::label('last', 'Last Name:', array('class' => 'col-sm-0 control-label')) }}
 				{{ Form::text('last', Input::old('last'), array('class' => 'form-control')) }}
-                @if($errors->has('last')) <div class="alert alert-danger" role="alert">{{ $errors->first('last') }}</div> @endif
+				@if($errors->has('last')) <div class="alert alert-danger" role="alert">{{ $errors->first('last') }}</div> @endif
 
 			</div>
 
 			<div class="form-group">
 				{{ Form::label('email', 'E-mail Address:', array('class' => 'col-sm-0 control-label')) }}
 				{{ Form::text('email', Input::old('email'), array('class' => 'form-control')) }}
-                @if($errors->has('email')) <div class="alert alert-danger" role="alert">{{ $errors->first('email') }}</div> @endif
+				@if($errors->has('email')) <div class="alert alert-danger" role="alert">{{ $errors->first('email') }}</div> @endif
 			</div>	
 			<hr>
 
@@ -44,14 +44,14 @@
 				{{ Form::text('username', Input::old('username'), array('class' => 'form-control')) }}
 				<p class="help-block">Please make sure your username is between 5 and 25 characters long and only contains
 					numbers, letters, dashes and underscores.</p>
-                @if($errors->has('username')) <div class="alert alert-danger" role="alert">{{ $errors->first('username') }}</div> @endif
+					@if($errors->has('username')) <div class="alert alert-danger" role="alert">{{ $errors->first('username') }}</div> @endif
 				</div>	
 
 				<div class="form-group">
 					{{ Form::label('password', 'Desired Password:', array('class' => 'col-sm-0 control-label')) }}
 					{{ Form::password('password', array('class' => 'form-control')) }}
 					<p class="help-block">Please make sure your password is at least 5 characters long.</p>
-	                @if($errors->has('password')) <div class="alert alert-danger" role="alert">{{ $errors->first('password') }}</div> @endif
+					@if($errors->has('password')) <div class="alert alert-danger" role="alert">{{ $errors->first('password') }}</div> @endif
 				</div>
 
 				<div class="form-group">
@@ -69,129 +69,61 @@
 				<div class="form-group">
 					{{ Form::label('experience', 'Please Enter Your Years of Experience in this field:') }}<br>
 					{{ Form::select('experience', array('0-1' => '0-1 Years', '1-5' => '1-5 Years', '5-10' => '5-10 Years', '10+' => '10+ Years')) }}
-                @if($errors->has('experience')) <div class="alert alert-danger" role="alert">{{ $errors->first('experience') }}</div> @endif
+					@if($errors->has('experience')) <div class="alert alert-danger" role="alert">{{ $errors->first('experience') }}</div> @endif
 				</div><br>
 
 				<div class='form-group'>
 					{{ Form::label('skills', 'What skills do you have pertaining to this field:', array('class' => 'col-sm-0 control-label')) }}
 					{{ Form::text('skills', Input::old('first'), array('class' => 'form-control')) }}
-                @if($errors->has('skills')) <div class="alert alert-danger" role="alert">{{ $errors->first('skills') }}</div> @endif
+					@if($errors->has('skills')) <div class="alert alert-danger" role="alert">{{ $errors->first('skills') }}</div> @endif
 				</div><br> 
-				<!-- ================================== -->
-
-				<div class='form-group'>
-					{{ Form::label('talent', 'What is your Role in the entertainment industry:') }}<br>
-<<<<<<< HEAD
-					{{ Form::select('talent', array(null => 'Please Select an option', 'talent' => 'I am the talent', 'agent' => 'I am an Agent or Manager')) }}
-					@if ($errors->has('talent')) <p class="help-block">{{ $errors->first('talent') }}</p> @endif
-				</div><br>
-
-				    @if($_GET === 'role=talent') 
-				    	<!-- talent Options -->
- 						<div class='form-group'>
-							{{ Form::label('dob', 'Please Enter Your Date Of Birth:', array('class' => 'col-sm-0 control')) }}
-							{{-- Form::date('dob') --}}
-							<br><input type='date' name='dob' class=''>
-						</div>
-
-
-						<div class="form-group">
-							{{ Form::label('bio', 'Biography:') }}
-							{{ Form::textarea('bio', '' , array('class' => 'form-control')) }}
-							@if ($errors->has('bio')) <p class="help-block">{{ $errors->first('bio') }}</p> @endif
-						</div><br>
-
-						<div class="form-group">
-							{{ Form::label('img', 'Please upload a profile photo') }}
-							{{ Form::file('img') }}
-							<p class="help-block">This step is optional.</p>
-						</div>	
-					
-				    @elseif($_GET === 'role=agent') 
-						<!-- agent options -->
-						<div class-"form-group">
-							{{ Form::label('company', 'What agency do you work with? :') }}
-							{{ Form::text('company', Input::old('company'), array('class' => 'form-control')) }}
-						</div><br>
-
-						<div class-"form-group">
-							{{ Form::label('bio', 'Biography:') }}
-							{{ Form::textarea('bio', '' , array('class' => 'form-control')) }}
-							@if ($errors->has('bio')) <p class="help-block">{{ $errors->first('bio') }}</p> @endif
-						</div><br>
-
-						<div class="form-group">
-							{{ Form::label('img', 'Please upload a profile photo') }}
-							{{ Form::file('img') }}
-							<p class="help-block">This step is optional.</p>
-						</div><br>
-					@else
-						<p>Please select an option.</p>
-					@endif	
-=======
-					{{ Form::select('talent', array('0' => 'I am an Agent or Manager', '1' => 'I am the talent')) }}
-                @if($errors->has('talent')) <div class="alert alert-danger" role="alert">{{ $errors->first('talent') }}</div> @endif
-				</div>
-
-				<div class='form-group'>
-					{{ Form::label('dob', 'Please Enter Your Date Of Birth:', array('class' => 'col-sm-0 control')) }}
-					{{-- Form::date('dob') --}}
-					<br><input type='date' name='dob' class=''>
-				</div>
-
-
-				<div class="form-group">
-					{{ Form::label('bio', 'Biography:') }}
-					{{ Form::textarea('bio', '' , array('class' => 'form-control')) }}
-                @if($errors->has('bio')) <div class="alert alert-danger" role="alert">{{ $errors->first('bio') }}</div> @endif
-				</div><br>
-
-				<div class="form-group">
-					{{ Form::label('img', 'Please upload a profile photo') }}
-					{{ Form::file('img') }}
-					<p class="help-block">This step is optional.</p>
-				</div>
-				<hr>
-
-				<!-- agent options -->
-				<div class-"form-group">
-					{{ Form::label('company', 'What agency do you work with? :') }}
-					{{ Form::text('company', Input::old('company'), array('class' => 'form-control')) }}
-                @if($errors->has('company')) <div class="alert alert-danger" role="alert">{{ $errors->first('company') }}</div> @endif
-				</div><br>
-
-				<div class-"form-group">
-					{{ Form::label('bio', 'Biography:') }}
-					{{ Form::textarea('bio', '' , array('class' => 'form-control')) }}
-                @if($errors->has('bioc')) <div class="alert alert-danger" role="alert">{{ $errors->first('bioc') }}</div> @endif
-				</div><br>
-
-				<div class="form-group">
-					{{ Form::label('img', 'Please upload a profile photo') }}
-					{{ Form::file('img') }}
-					<p class="help-block">This step is optional.</p>
-				</div>
->>>>>>> master
-
-				<div class="form-group">
-					{{ Form::submit('Create Account', ['class' => 'btn btn-default']) }}
-				</div>
-				    
-
+				<!-- ================================== -->				
 		
-		
-	{{ Form::close() }}
+			<div class='form-group'>
+				{{ Form::label('dob', 'Please Enter Your Date Of Birth:', array('class' => 'col-sm-0 control')) }}
+				{{-- Form::date('dob') --}}
+				<br><input type='date' name='dob' class=''>
+			</div>
 
-	<script>
+			@elseif($_GET === 'role=agent') 
+			<!-- agent options -->
+			<div class-"form-group">
+				{{ Form::label('company', 'What agency do you work with? :') }}
+				{{ Form::text('company', Input::old('company'), array('class' => 'form-control')) }}
+			</div><br>
 
-	$talentInputs = $('#talentInputs');
+			<div class="form-group">
+				{{ Form::label('bio', 'Biography:') }}
+				{{ Form::textarea('bio', '' , array('class' => 'form-control')) }}
+				@if($errors->has('bio')) <div class="alert alert-danger" role="alert">{{ $errors->first('bio') }}</div> @endif
+			</div><br>
 
-	$talentInputs.hide();
+			<div class="form-group">
+				{{ Form::label('img', 'Please upload a profile photo') }}
+				{{ Form::file('img') }}
+				<p class="help-block">This step is optional.</p>
+			</div>
+			<hr>
+			
+			<div class="form-group">
+				{{ Form::submit('Create Account', ['class' => 'btn btn-default']) }}
+			</div>
 
 
-	$agentInputs = $('#agentInputs');
 
-	$agentInputs.hide();
+
+			{{ Form::close() }}
+
+			<script>
+
+			$talentInputs = $('#talentInputs');
+
+			$talentInputs.hide();
+
+
+			$agentInputs = $('#agentInputs');
+
+			$agentInputs.hide();
 
 //maybe use .change() or .val() or :selected
 function showTalent(){
