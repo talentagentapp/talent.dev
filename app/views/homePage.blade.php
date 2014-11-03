4131915a5e00a1f9cfd37c5e358df8000ea24f80
Adam Vega <adam.j.vega@hotmail.com>
2014-11-03 11:23:00 -0600

    cocommit with homepage showing user info

--- app/views/homePage.blade.php
+++ app/views/homePage.blade.php
@@ -2,19 +2,18 @@
 
 @section('title', 'Homepage')
 <style>
-h3
-{
-    text-decoration:underline;
-}
+    h3
+    {
+        text-decoration:underline;
+    }
 </style>
 
 @section('content')
 <div class='container'>
     <div class='row'>
         <div id='featured-talent'  class='col-md-8'>
-        <h1>Featured User</h1>
+            <h1>Featured User</h1>
             <div class='row'>
-
                 <div class='col-md-8'>
                     <h1>{{{ $user->first . ' ' . $user->last }}}</h1>
                     <img src="{{ $user->img }}" class='img-thumbnail img-responsive'>
@@ -30,7 +29,18 @@ h3
                     <p>Gender: {{{ $user->gender_str }}}</p>
                 </div>
             </div>
+            <br>
+            <hr>
+            <h1>Your Info</h1>
+            <p>{{ Auth::user()->first }} {{ Auth::user()->last }}</p>
+            <div class='col-md-8'>
+                <h1>{{{ Auth::user()->first . ' ' . Auth::user()->last }}}</h1>
+                <img src="{{ Auth::user()->img }}" class='img-thumbnail img-responsive'>
+                <h4><a href="mailto:{{{ Auth::user()->email }}}">{{{ Auth::user()->email }}}</a></h4>
+                <p>{{{ Auth::user()->bio }}}</p>
+            </div>
         </div>
+
         <div class='col-md-4' style='background-color:#A19A97;'>
             @foreach ($gigs as $gig)
             <h3>{{{$gig->name}}}</h3>
@@ -39,9 +49,13 @@ h3
             @endforeach
         </div>
     </div>
-    <div class="row">
-        
-    </div>
+
+
+
+
+
+
+
     <div id='bottom-row' class='row'>
         <div class='col-md-12'>
             <p>so much stuff in this row will be cool man someday</p>
